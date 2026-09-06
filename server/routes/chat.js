1c9eaf8d5574733701b0a62c204b44bcf45aceb6
Narayan shukla <nnnshukla@gmail.com>
2026-09-06 20:09:55 +0530

    Delete server directory

--- server/routes/chat.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { chatWithDocument, getChatHistory } = require('../controllers/chatController');
-
-router.post('/', chatWithDocument);
-router.get('/:docId', getChatHistory);
-
-module.exports = router;
